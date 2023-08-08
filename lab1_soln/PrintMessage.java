4639133373795e681b2fb51211935c72f37373db
jeba <jr.gamachu@gmail.com>
2023-08-08 12:51:24 +0530

    Solution of question-2

---
+++ lab1_soln/PrintMessage.java
@@ -0,0 +1,18 @@
+/**
+ * Q2. Write a Java program to print a message “Welcome (Your name
+ * here) to Java Programming”, where your name is stored in a
+ * variable.
+ *
+ * @author: Gamachu (2175052)
+ * @date: Jul 25 2023 (Tues)
+ * @lab1: KIIT, Campus 1 (sca)
+ * @instructor: Mr. Abinash Tripathy
+ */
+
+ class PrintMessage {
+ 	static public void main(String[] args) {
+ 		String myName = "Gamachu";
+ 		System.out.println("Welcome " + myName + " to Java Programming");
+ 	}
+ }
+ 	
