22d6a806bce8b3f934dc45ca412d11b8ecaf9e38
jeba <jr.gamachu@gmail.com>
2023-08-08 12:46:49 +0530

    Solution of question-1

---
+++ lab1_soln/AboutMe.java
@@ -0,0 +1,17 @@
+/**
+ * Q1. Write a simple Java program to print three characteristics about
+ * yourself.
+ *
+ * @author: Gamachu (2175052)
+ * @date: Jul 25 2023 (Tues)
+ * @lab1: KIIT, Campus 1 (sca)
+ * @instructor: Mr. Abinash Tripathy
+ */
+
+ class AboutMe {
+ 	public static void main(String args[]) {
+ 		System.out.println("I'm nobody - you can call me Geme");
+ 		System.out.println("I love doing Taek-won-do");
+ 		System.out.println("My mother tongue is Afan Oromo");
+ 	}
+ }
