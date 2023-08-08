2d290ade2e639aaaaf21a3cc267d45477f71a1c9
jeba <jr.gamachu@gmail.com>
2023-08-08 12:47:18 +0530

    Solution of question-2

---
+++ lab1_soln/ArithmeticOperations.java
@@ -0,0 +1,35 @@
+/**
+ * Q4. Declare two integer variables then apply +, -, *, /, % operations.
+ * Also declare suitable variable to store the result.
+ * 
+ * @author: Gamachu (2175052)
+ * @date: Jul 25 2023 (Tues)
+ * @lab1: KIIT, Campus 1 (sca)
+ * @instructor: Mr. Abinash Tripathy 
+ */
+
+ class ArithmeticOperations {
+ 	public static void main(String [] args) {
+ 		int firstInt = 80;
+ 		int secondInt = 6;
+ 		
+ 		// Addition operator
+ 		int sum = firstInt + secondInt;
+ 		// Subraction operator
+ 		int difference = firstInt - secondInt;
+ 		// Multiplication operator
+ 		int product = firstInt * secondInt;
+ 		// Division operator
+ 		int quotient = firstInt / secondInt;
+ 		// Modulus operator
+ 		int remainder = firstInt % secondInt;
+ 		
+ 		System.out.printf("For firstInt = %d and secondInt = %d\n", firstInt, secondInt);   // yeah! we can also use printf in java :-)
+ 		System.out.println("Sum: " + sum);
+ 		System.out.println("Difference: " + difference);
+ 		System.out.println("Product: " + product);
+ 		System.out.println("Quotient: " + quotient);
+ 		System.out.println("Remainder: " + remainder);
+ 	}
+ }
+ 		
